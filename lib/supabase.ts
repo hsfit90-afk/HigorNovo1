26f88df04577dfe3019f25e211e26ef48385e773
hsfit90-afk <hsfit90@gmail.com>
2026-06-10 21:54:39 -0300

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,1 +1,8 @@
+import { createClient } from '@supabase/supabase-js';
 
+// Aqui o código vai buscar as chaves secretas que guardamos no cofre da Vercel
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+// Criando a conexão com o banco de dados
+export const supabase = createClient(supabaseUrl, supabaseKey);
